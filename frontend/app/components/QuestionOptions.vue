b7b6ccc70e9b8e5fde1983c925975f442e98351f
David Licen <david@artnetik.si>
2026-08-17 11:54:15 +0200

    Move options code to separate component

---
+++ frontend/app/components/QuestionOptions.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+interface Option {
+  id: string;
+  label: string;
+}
+
+defineProps<{
+  options: Option[];
+}>();
+</script>
+
+<template>
+  <div class="flex flex-wrap justify-center items-center gap-4">
+    <label
+      v-for="option in options"
+      :key="option.id"
+      class="flex items-center gap-3 cursor-pointer px-4 py-2 rounded-md has-checked:ring-(--brass) has-checked:ring-2 hover:bg-yellow-500/20 transition-colors"
+    >
+      <input type="radio" class="peer sr-only" :name="id" :value="option.id" />
+      <span
+        class="size-5 shrink-0 rounded-full border-2 border-(--brass) grid place-items-center after:content-[''] after:size-2.5 after:rounded-full after:bg-(--brass) after:scale-0 after:transition-transform peer-checked:after:scale-100"
+      />
+      <span class="text-lg">{{ option.label }}</span>
+    </label>
+  </div>
+</template>
--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -23,25 +23,7 @@ const answers = ref({});
           {{ currentQuestion.title }}
         </div>
 
-        <div class="flex flex-wrap justify-center items-center gap-4">
-          <label
-            v-for="option in currentQuestion.options"
-            :key="option.id"
-            class="flex items-center gap-3 cursor-pointer px-4 py-2 rounded-md has-checked:ring-(--brass) has-checked:ring-2 hover:bg-yellow-500/20 transition-colors"
-          >
-            <input
-              type="radio"
-              class="peer sr-only"
-              :name="currentQuestion.id"
-              :value="option.id"
-              v-model="answers[currentQuestion.id]"
-            />
-            <span
-              class="size-5 shrink-0 rounded-full border-2 border-(--brass) grid place-items-center after:content-[''] after:size-2.5 after:rounded-full after:bg-(--brass) after:scale-0 after:transition-transform peer-checked:after:scale-100"
-            />
-            <span class="text-lg">{{ option.label }}</span>
-          </label>
-        </div>
+        <QuestionOptions :options="currentQuestion.options" />
       </div>
     </section>
   </main>
