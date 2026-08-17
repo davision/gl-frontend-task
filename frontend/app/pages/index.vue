5029727404199cc5fe62fed6d3b460d37575aa9f
David Licen <david@artnetik.si>
2026-08-17 15:37:46 +0200

    Improve annimations

--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -25,68 +25,76 @@ const {
         <h1 class="text-center text-4xl! font-bold">Casino Finder</h1>
         <div class="w-36 h-px bg-(--brass) my-16"></div>
 
-        <template v-if="!showResults && currentQuestion">
-          <QuestionNumbers
-            :questions="questions"
-            :current-question="currentQuestion"
-          />
+        <Transition name="reveal" mode="out-in">
+          <div
+            class="flex flex-col items-center w-full"
+            v-if="!showResults && currentQuestion"
+          >
+            <QuestionNumbers
+              :questions="questions"
+              :current-question="currentQuestion"
+            />
 
-          <Transition name="reveal" mode="out-in">
-            <div
-              :key="currentQuestion.id"
-              class="flex w-full flex-col items-center"
-            >
-              <div class="text-center font-bold text-2xl mt-16 mb-8">
-                {{ currentQuestion.title }}
+            <Transition name="reveal" mode="out-in">
+              <div
+                :key="currentQuestion.id"
+                class="flex w-full flex-col items-center"
+              >
+                <div class="text-center font-bold text-2xl mt-16 mb-8">
+                  {{ currentQuestion.title }}
+                </div>
+
+                <QuestionOptions
+                  :name="currentQuestion.id"
+                  :options="currentQuestion.options"
+                  v-model="answers[currentQuestion.id]"
+                />
               </div>
+            </Transition>
 
-              <QuestionOptions
-                :name="currentQuestion.id"
-                :options="currentQuestion.options"
-                v-model="answers[currentQuestion.id]"
-              />
+            <div class="mt-12 flex items-center gap-4">
+              <button
+                v-if="!isFirst"
+                type="button"
+                class="px-4 py-2 flex items-center gap-2 text-white/60 cursor-pointer rounded-md border border-white/20 disabled:opacity-40"
+                :disabled="isFirst"
+                @click="goBack"
+              >
+                Back
+              </button>
+              <button
+                type="button"
+                class="cursor-pointer px-4 py-2 rounded-md bg-(--brass) text-yellow-900 font-bold disabled:opacity-40"
+                :disabled="!canGoNext"
+                @click="goNext"
+              >
+                {{ isLast ? "See recommendation" : "Next" }}
+              </button>
             </div>
-          </Transition>
-
-          <div class="mt-12 flex items-center gap-4">
-            <button
-              v-if="!isFirst"
-              type="button"
-              class="px-4 py-2 flex items-center gap-2 text-white/60 cursor-pointer rounded-md border border-white/20 disabled:opacity-40"
-              :disabled="isFirst"
-              @click="goBack"
-            >
-              Back
-            </button>
-            <button
-              type="button"
-              class="cursor-pointer px-4 py-2 rounded-md bg-(--brass) text-yellow-900 font-bold disabled:opacity-40"
-              :disabled="!canGoNext"
-              @click="goNext"
-            >
-              {{ isLast ? "See recommendation" : "Next" }}
-            </button>
           </div>
-        </template>
 
-        <template v-else-if="recommendation">
-          <h3 class="text-center text-3xl! font-bold mb-12">
-            Based on your answers, we recommend you to try:
-          </h3>
+          <div
+            v-else="recommendation"
+            class="flex flex-col items-center w-full"
+          >
+            <h3 class="text-center text-3xl! font-bold mb-12">
+              Based on your answers, we recommend you to try:
+            </h3>
 
-          <CasinoCard :casino="recommendation" />
+            <CasinoCard :casino="recommendation" />
 
-          <div class="mt-8">
-            <button
-              type="button"
-              class="cursor-pointer px-4 py-2 flex items-center gap-2 rounded-md bg-(--brass) text-yellow-900 font-bold disabled:opacity-40"
-              @click="startOver"
-            >
-              <ArrowBack class="w-4 h-4" />
-              Start over
-            </button>
+            <div class="mt-8">
+              <button
+                type="button"
+                class="cursor-pointer px-4 py-2 flex items-center gap-2 rounded-md bg-(--brass) text-yellow-900 font-bold disabled:opacity-40"
+                @click="startOver"
+              >
+                <ArrowBack class="w-4 h-4" />
+                Start over
+              </button>
+            </div>
           </div>
-        </template>
+        </Transition>
       </div>
     </section>
   </main>
